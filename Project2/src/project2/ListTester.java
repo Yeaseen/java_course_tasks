ced818e03cb28571428027efd0201303281d0d7f
bisonBios <yeaseen.arafat@seu.edu.bd>
2022-10-23 12:50:41 +0600

    sophisticated test cases added

--- Project2/src/project2/ListTester.java
+++ Project2/src/project2/ListTester.java
@@ -8,13 +8,15 @@ public class ListTester {
 		UnsortedWordList a = new UnsortedWordList();
 		SortedWordList b = new SortedWordList();
 		
-		a.append("What"); 
-		
-		b.add("What");
-		
-		a.append("Now");
-		
-		b.add("Now");
+		a.append("Bat"); 
+		b.add("Bat");		
+		a.append("Apple");		
+		b.add("Apple");
+		a.append("CAST");		
+		b.add("CAST");
+
+		a.append("01DOLL");
+		b.add("01DOLL");
 		
 		
 		
